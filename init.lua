3895063e3aa11c716323a75f06f3a966ab845f96
Syed Ali Raza <isyedaliraza@gmail.com>
2024-12-02 10:36:03 +0000

    Add xmlformatter

--- init.lua
+++ init.lua
@@ -717,6 +717,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        xml = { 'xmlformatter' },
       },
     },
   },
