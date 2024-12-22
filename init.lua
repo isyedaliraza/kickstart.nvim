f245fa241b51a267ab8fbd52b53aff7aee721075
Syed Ali Raza <isyedaliraza@gmail.com>
2024-12-22 08:49:04 +0000

    Save changes

--- init.lua
+++ init.lua
@@ -706,7 +706,7 @@ require('lazy').setup({
           lsp_format_opt = 'fallback'
         end
         return {
-          timeout_ms = 500,
+          timeout_ms = 1500,
           lsp_format = lsp_format_opt,
         }
       end,
