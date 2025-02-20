75c1d670878d43d7455ae9359ff8692e499159ef
Syed Ali Raza <isyedaliraza@gmail.com>
2025-02-20 14:16:00 +0000

    Fix venv-selector plugin

--- lua/custom/plugins/venv-selector.lua
+++ lua/custom/plugins/venv-selector.lua
@@ -1,11 +1,15 @@
 return {
   'linux-cultist/venv-selector.nvim',
   dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
-  opts = {
-    -- Your options go here
-    -- name = "venv",
-    -- auto_refresh = false
-  },
+  config = function()
+    require('venv-selector').setup {
+      -- Your options go here
+      -- name = "venv",
+      -- auto_refresh = false
+      anaconda_base_path = '/opt/miniconda3/',
+      anaconda_envs_path = '/opt/miniconda3/envs/',
+    }
+  end,
   event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
   keys = {
     -- Keymap to open VenvSelector to pick a venv.
